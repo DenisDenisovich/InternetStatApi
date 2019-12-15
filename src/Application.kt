57e266da4303ad4dd9a77bda7d3b611651ce2056
vladDenisov <vl.denisov.v@gmail.com>
2019-12-16 01:18:37 +0300

    remove test

--- src/Application.kt
+++ src/Application.kt
@@ -10,19 +10,41 @@ import kotlinx.html.*
 import kotlinx.css.*
 import io.ktor.client.*
 import io.ktor.client.engine.apache.*
+import io.ktor.client.request.request
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.lang.StringBuilder
 
 fun main(args: Array<String>) {
     val server = embeddedServer(Netty, port = 8080) {
         routing {
             get("/") {
-                call.respondText("Hello World!", ContentType.Text.Plain)
+                val userID = "user${(Math.random() * 1000).toInt()}"
+                call.respondText("Hello World! $userID", ContentType.Text.Plain)
+                Db.addUser(userID)
             }
             get("/demo") {
-                call.respondText("HELLO WORLD!")
+
+                val sb = StringBuilder()
+                Db.getUsers().forEach {
+                    sb.appendln(it)
+                }
+                call.respondText("HELLO WORLD!\nsb")
             }
         }
     }
     server.start(wait = true)
+}
+
+private fun addTestUSer() {
+    val user = "USer1"
+    Db.addUser(user)
+}
+
+private fun getUser() {
+
 }
---
+++ src/Db.kt
@@ -0,0 +1,70 @@
+package com.example.network.statistic
+
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.lang.Exception
+import java.lang.StringBuilder
+
+object Db {
+
+    init {
+        Database.connect("jdbc:sqlite:/data/data.db", "org.sqlite.JDBC")
+    }
+
+    object Users : Table() {
+        val id = varchar("id", 50).autoIncrement().primaryKey() // Column<String>
+        val userId = varchar("user_id", 50)
+    }
+
+    object NetworkData : Table() {
+        val id = varchar("id", 50).autoIncrement().primaryKey() // Column<String>
+        val userId = varchar("user_id", 50)
+        val data = varchar("data", 300)
+    }
+
+    object UserApplications : Table() {
+        val id = varchar("id", 50).autoIncrement().primaryKey() // Column<String>
+        val userId = varchar("user_id", 50)
+        val apps = text("apps")
+    }
+
+    fun addApps(user: String, apps: ArrayList<Pair<Int, String>>) {
+        UserApplications.update({ UserApplications.userId eq user }) {
+            val sb = StringBuilder()
+            apps.forEach {
+                sb.append("{${it.first}, ${it.second}}, ")
+            }
+            it[UserApplications.apps] = sb.toString()
+        }
+    }
+
+    fun addNetworkData(user: String, data: String) {
+        NetworkData.insert {
+            it[NetworkData.userId] = user
+            it[NetworkData.data] = data
+        }
+    }
+
+    fun addUser(user: String) {
+        val existedUserID = try {
+            transaction { Users.select { Users.userId eq user } }.single()[Users.userId]
+        } catch (e: Exception) {
+            null
+        }
+        if (existedUserID != null) {
+            Users.insert {
+                it[Users.userId] = user
+            }
+        }
+    }
+
+    fun getUsers(): ArrayList<String> {
+        val users = arrayListOf<String>()
+        transaction {
+            Users.selectAll()
+        }.forEach {
+            users.add(it[Users.userId])
+        }
+        return users
+    }
+}
