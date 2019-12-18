022b76add24597b8d5b0a0c358c8ba53c547b7ba
vladDenisov <vl.denisov.v@gmail.com>
2019-12-18 22:59:08 +0300

    fix query and

--- src/com/example/network/statistic/domian/networdata/GetNetworkDataUseCase.kt
+++ src/com/example/network/statistic/domian/networdata/GetNetworkDataUseCase.kt
@@ -4,12 +4,7 @@ import com.example.network.statistic.db.Db
 import com.example.network.statistic.domian.UseCase
 import com.example.network.statistic.models.NetworkData
 import com.example.network.statistic.models.NetworkPeriod
-import org.jetbrains.exposed.sql.SortOrder
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.lessEq
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.greaterEq
-import org.jetbrains.exposed.sql.and
-import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class GetNetworkDataUseCase(
@@ -19,12 +14,17 @@ class GetNetworkDataUseCase(
     private val endTime: Long
 ) : UseCase<ArrayList<NetworkData>>() {
 
+    val data = ArrayList<NetworkData>()
+
     override fun execute(): ArrayList<NetworkData> {
+        getAll()
         val result = arrayListOf<NetworkData>()
-        val condition = Db.NetworkData.userId eq user and
-                (Db.NetworkData.period eq period.name) and
-                (Db.NetworkData.timestamp greaterEq startTime) and
-                (Db.NetworkData.timestamp lessEq endTime)
+        val condition = Op.build {
+            (Db.NetworkData.userId eq user) and
+                    (Db.NetworkData.period eq period.name) and
+                    (Db.NetworkData.timestamp greaterEq startTime) and
+                    (Db.NetworkData.timestamp greaterEq endTime)
+        }
         transaction {
             Db.NetworkData
                 .select(condition)
@@ -41,5 +41,20 @@ class GetNetworkDataUseCase(
                 }
         }
         return result
+    }
+
+    private fun getAll() {
+        transaction {
+            Db.NetworkData.selectAll().forEach {
+                data.add(
+                    NetworkData(
+                        it[Db.NetworkData.userId],
+                        it[Db.NetworkData.timestamp],
+                        NetworkPeriod.valueOf(it[Db.NetworkData.period]),
+                        it[Db.NetworkData.data]
+                    )
+                )
+            }
+        }
     }
 }
